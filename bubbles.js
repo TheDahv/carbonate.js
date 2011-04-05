6b878c6f701271f1fcb3e925664f56e363126fb5
David Pierce <david.dean.pierce@gmail.com>
2011-04-04 18:01:16 -0700

    Algorithmic improvements to the drawing function.

--- bubbles.js
+++ bubbles.js
@@ -1,5 +1,13 @@
-var fps = 1;
+var fps = 25;
 var timeInterval = 1000 / fps;
+
+var head = function (arr) { return arr[0]; };
+var each = function (f, arr) {
+  if (arr.length > 0) {
+    f(head(arr));
+    each(f, arr.slice(1));
+  }
+}
 
 // Helper functions in the bubble namespace
 // Create global namespace variable called 'bubble_ns'
@@ -11,10 +19,12 @@ var bubble_ns = {
   BUBBLE_SPEED_MIN: 50,
   BUBBLE_WIDTH_MAX: 10,
   BUBBLE_WIDTH_MIN: 3,
+  BUBBLE_DISTANCE_MIN: 6,
+  BUBBLE_DISTANCE_MAX: 15,
   CANVAS_X_MAX: -1,
   CANVAS_Y_MAX: -1,
   bubble_id_count: 1,
-  num_bubbles_at_a_time: 3
+  num_bubbles_at_a_time: 1
 };
 
 var beer_canvas = null;
@@ -25,7 +35,10 @@ var clearCanvas = function () {
 };
 
 var drawBubbles = function () {
-  bubble_ns.bubble_holder.map(function (bubble) { if (bubble.bubble) { bubble.bubble.drawBubble(); } });
+  each(
+    function(bubble) { if(bubble.bubble) { bubble.bubble.drawBubble(); } },
+    bubble_ns.bubble_holder
+  );
 };
 
 var Point = function (x, y) {
@@ -50,6 +63,9 @@ var generateYValue = function () {
   return numberGenerator(0, bubble_ns.CANVAS_Y_MAX);  
 };
 
+var generateDistance = function () {
+  return numberGenerator(bubble_ns.BUBBLE_DISTANCE_MIN, bubble_ns.BUBBLE_DISTANCE_MAX);
+}
 
 var addBubble = function (bubble) {
   bubble_ns.bubble_holder.push({
@@ -74,13 +90,9 @@ function BeerBubble(id, centerPoint, width) {
   this.centerPoint = centerPoint;
   this.width = width;
   
-  this.intervalId = -1;  
+  this.distance = generateDistance();
   
-  var that = this;
-  
-  this.setIntervalId = function (id) {
-    that.intervalId = id;
-  };  
+  var that = this;  
   
   this.getBubbleId = function () {
     return that.bubbleId;
@@ -90,44 +102,23 @@ function BeerBubble(id, centerPoint, width) {
     var width = that.width, 
       centerPoint = that.centerPoint,  
       context = beer_context;
-    
-    // Stop animating when it floats past the top of the page
-    if (that.intervalId > 0 && that.centerPoint.y < 0) {			
-      clearInterval(that.intervalId);
-      
-      // Mark this bubble for deletion
-      removeBubble(that.bubbleId);
-      
-      // Stop executing code
-      return;
-    }
         
     context.beginPath();
-
     context.arc(centerPoint.x, centerPoint.y, width, 0, Math.PI * 2, true);
-
     context.closePath();
     context.stroke();
   };
 
   this.bubble_float = function () {
-    var width = that.width, 
-      y = that.centerPoint.y;
+    // Stop animating when it floats past 
+    // the top of the page and kill the bubble
+    if(that.centerPoint.y < 0) {
+      removeBubble(that.bubbleId);
+      return;
+    }
     
-    // Take the existing frame out
-    beer_context.fillStyle = "#00f";
-    beer_context.clearRect(
-      that.centerPoint.x - width - 1, 
-      y - width - 1, 
-      width * 2 + 2, 
-      width * 2 + 2
-    );
-
     // Raise the position a little bit
-    that.centerPoint.y = y - 5;
-		
-    // Redraw
-    that.drawBubble();
+    that.centerPoint.y -= that.distance;
   };        
 }
 
@@ -149,28 +140,37 @@ var startFloating = function () {
 };
 
 var init_bubbles = function () {  
-  setInterval(
+  master_bubble_interval = setInterval(
     function () {      
-      var i, id;
+      var i;
       // Make some new bubbles
       for (i = 0; i < bubble_ns.num_bubbles_at_a_time; i += 1) {
+        var bubble, bubbleId, intervalId;      
+        
         // Grab an id
-        id = bubble_ns.bubble_id_count;
+        bubbleId = bubble_ns.bubble_id_count;
         bubble_ns.bubble_id_count += 1;
         
         // Create a new bubble and add it to the holder
-        addBubble(
-          new BeerBubble(
-            id,
-            new Point(
+        var bubble = new BeerBubble(
+          bubbleId,
+          new Point(
               generateXValue(),
               generateYValue()
-            ),
-            generateWidth()
-          )
-        );      
-        // Kick off the floating
-        startFloating();
+          ),
+          generateWidth()
+        );
+        addBubble(bubble);
+        
+        clearCanvas();      
+        // Kick off the floating and drawing
+        each(
+          function (bubble) { 
+            bubble.bubble.bubble_float();
+            bubble.bubble.drawBubble(); 
+          },
+          bubble_ns.bubble_holder
+        );
       }
     }, 
     timeInterval
