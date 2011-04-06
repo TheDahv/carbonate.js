c7e3da075d4ad33bcfc8edb948e7094f091f3fd8
David Pierce <david.dean.pierce@gmail.com>
2011-04-05 23:10:12 -0700

    Code organization. Function object creation instead of constructors. Cut down on globals

--- bubbles.js
+++ bubbles.js
@@ -1,222 +1,212 @@
-var fps = 25;
-var timeInterval = 1000 / fps;
+var debug_framework;
+var master_bubble_interval;
 
-var head = function (arr) { return arr[0]; };
-var each = function (f, arr) {
-  if (arr.length > 0) {
-    f(head(arr));
-    each(f, arr.slice(1));
-  }
-}
+var stop = function () { clearInterval(master_bubble_interval); };
 
-// Helper functions in the bubble namespace
-// Create global namespace variable called 'bubble_ns'
-var bubble_ns = {
-  bubble_holder: [],
-  canvas_ref: undefined,
-  canvas_bottom: 0,
-  BUBBLE_SPEED_MAX: 100,
-  BUBBLE_SPEED_MIN: 50,
-  BUBBLE_WIDTH_MAX: 10,
-  BUBBLE_WIDTH_MIN: 3,
-  BUBBLE_DISTANCE_MIN: 6,
-  BUBBLE_DISTANCE_MAX: 15,
-  CANVAS_X_MAX: -1,
-  CANVAS_Y_MAX: -1,
-  bubble_id_count: 1,
-  num_bubbles_at_a_time: 1
-};
-
-var beer_canvas = null;
-var beer_context = null;
-
-var clearCanvas = function () {
-  beer_context.clearRect(0, 0, beer_canvas.width, beer_canvas.height);
-};
-
-var drawBubbles = function () {
-  each(
-    function(bubble) { if(bubble.bubble) { bubble.bubble.drawBubble(); } },
-    bubble_ns.bubble_holder
-  );
-};
-
-var Point = function (x, y) {
-  this.x = x;
-  this.y = y;
-};
-
-var numberGenerator = function (min_value, max_value) {
-  var number = Math.floor(Math.random() * max_value);
-  return (number < min_value ? min_value : number);
-};
-
-var generateWidth = function () {
-  return numberGenerator(bubble_ns.BUBBLE_WIDTH_MIN, bubble_ns.BUBBLE_WIDTH_MAX);
-};
-
-var generateXValue = function () {
-  return numberGenerator(0, bubble_ns.CANVAS_X_MAX);
-};
-
-var generateYValue = function () {
-  return numberGenerator(0, bubble_ns.CANVAS_Y_MAX);  
-};
-
-var generateDistance = function () {
-  return numberGenerator(bubble_ns.BUBBLE_DISTANCE_MIN, bubble_ns.BUBBLE_DISTANCE_MAX);
-}
-
-var addBubble = function (bubble) {
-  bubble_ns.bubble_holder.push({
-    bubble_id: bubble.getBubbleId(),
-    bubble: bubble
-  });  
-};
-
-var removeBubble = function (bubbleId) {
-  var i;
-  for (i = 0; i < bubble_ns.bubble_holder.length; i += 1) {
-    if (bubble_ns.bubble_holder[i].bubble_id === bubbleId) {
-      bubble_ns.bubble_holder.splice(i, 1);
-      return;
-    }
-  }
-};
-
-
-function BeerBubble(id, centerPoint, width) {
-  this.bubbleId = id;
-  this.centerPoint = centerPoint;
-  this.width = width;
-  
-  this.distance = generateDistance();
-  
-  var that = this;  
-  
-  this.getBubbleId = function () {
-    return that.bubbleId;
-  };
-  
-  this.drawBubble = function () {
-    var width = that.width, 
-      centerPoint = that.centerPoint,  
-      context = beer_context;
-        
-    context.beginPath();
-    context.arc(centerPoint.x, centerPoint.y, width, 0, Math.PI * 2, true);
-    context.closePath();
-    context.stroke();
-  };
-
-  this.bubble_float = function () {
-    // Stop animating when it floats past 
-    // the top of the page and kill the bubble
-    if(that.centerPoint.y < 0) {
-      removeBubble(that.bubbleId);
-      return;
-    }
-    
-    // Raise the position a little bit
-    that.centerPoint.y -= that.distance;
-  };        
-}
-
-var resetHeight = function () {
-  beer_canvas.setAttribute('width', document.body.offsetWidth);
-  beer_canvas.setAttribute('height', document.body.offsetHeight);
-  bubble_ns.canvas_bottom = beer_canvas.height;
-};
-
-
-var startFloating = function () {
-  bubble_ns.bubble_holder.map(
-    function (bubble) { 
-      var speed = numberGenerator(bubble_ns.BUBBLE_SPEED_MIN, bubble_ns.BUBBLE_SPEED_MAX),
-        id = setInterval(bubble.bubble.bubble_float, speed);
-      bubble.bubble.setIntervalId(id); 
-    }
-  );
-};
-
-var init_bubbles = function () {  
-  master_bubble_interval = setInterval(
-    function () {      
-      var i;
-      // Make some new bubbles
-      for (i = 0; i < bubble_ns.num_bubbles_at_a_time; i += 1) {
-        var bubble, bubbleId, intervalId;      
-        
-        // Grab an id
-        bubbleId = bubble_ns.bubble_id_count;
-        bubble_ns.bubble_id_count += 1;
-        
-        // Create a new bubble and add it to the holder
-        var bubble = new BeerBubble(
-          bubbleId,
-          new Point(
-              generateXValue(),
-              generateYValue()
-          ),
-          generateWidth()
-        );
-        addBubble(bubble);
-        
-        clearCanvas();      
-        // Kick off the floating and drawing
-        each(
-          function (bubble) { 
-            bubble.bubble.bubble_float();
-            bubble.bubble.drawBubble(); 
-          },
-          bubble_ns.bubble_holder
-        );
+(function () {
+  var beer_framework = {
+    bubble_holder: [],
+    canvas_ref: undefined,
+    canvas_bottom: 0,
+    BUBBLE_SPEED_MAX: 100,
+    BUBBLE_SPEED_MIN: 50,
+    BUBBLE_WIDTH_MAX: 10,
+    BUBBLE_WIDTH_MIN: 3,
+    BUBBLE_DISTANCE_MIN: 6,
+    BUBBLE_DISTANCE_MAX: 15,
+    CANVAS_X_MAX: -1,
+    CANVAS_Y_MAX: -1,
+    bubble_id_count: 1,
+    num_bubbles_at_a_time: 1,
+    fps: 25,
+    timeInterval: function () { return 1000 / this.fps; },
+    beer_canvas: null,
+    beer_context: null,
+    clearCanvas: function () {
+      this.beer_context.clearRect(0, 0, this.beer_canvas.width, this.beer_canvas.height);
+    },
+    head: function (arr) { return arr[0]; },
+    each: function (f, arr) {
+      if (arr.length > 0) {
+        f(this.head(arr));
+        this.each(f, arr.slice(1));
       }
-    }, 
-    timeInterval
-  );
-};
-
-$(function () {
-  // Insert the CSS
-  $('head').append('<style type="text/css">#bubble_canvas{ position: absolute; z-index: -1; }</style>')
-
-  // Insert Canvas into content area
-  $('body').prepend('<canvas id="bubble_canvas"></canvas>');
-
-  beer_canvas = document.getElementById('bubble_canvas');
-  // Check for canvas support
-  if (beer_canvas.getContext) { commenceCarbonation(); }
-});
-
-var commenceCarbonation = function () {
-    resetHeight();
-    bubble_ns.CANVAS_X_MAX = beer_canvas.width;
-    bubble_ns.CANVAS_Y_MAX = beer_canvas.height;    
+    },
+    numberGenerator: function (min_value, max_value) {
+      var number = Math.floor(Math.random() * max_value);
+      return (number < min_value ? min_value : number);
+    },
+    generateWidth: function () {
+      return this.numberGenerator(this.BUBBLE_WIDTH_MIN, this.BUBBLE_WIDTH_MAX);
+    },
+    generateXValue: function () {
+      return this.numberGenerator(0, this.CANVAS_X_MAX);
+    },
+    generateYValue: function () {
+      return this.numberGenerator(0, this.CANVAS_Y_MAX);
+    },
+    generateDistance: function () {
+      return this.numberGenerator(this.BUBBLE_DISTANCE_MIN, this.BUBBLE_DISTANCE_MAX);
+    },
+    makePoint: function (x, y) {
+      return {
+        x: x,
+        y: y
+      }
+    },
+    addBubble: function (bubble) {
+      this.bubble_holder.push(bubble);
+    },
+    removeBubble: function (bubbleId) {
+      var i, that = this;
+      for(i = 0; i < that.bubble_holder.length; i += 1) {
+        if(that.bubble_holder[i].getBubbleId() === bubbleId) {
+          that.bubble_holder.splice(i, 1);
+          return;
+        }
+      }
+    },
+    makeBubble: function (id, centerPoint, width) {
+      var that = this;
+      return {
+        bubbleId: id,
+        centerPoint: centerPoint,
+        width: width,
+        distance: function () { return that.generateDistance(); },
+        getBubbleId: function () { return this.bubbleId; },
+        drawBubble: function () {
+          var width = this.width,
+            centerPoint = this.centerPoint,
+            context = that.beer_context;
+          context.beginPath();
+          context.arc(centerPoint.x, centerPoint.y, width, 0, Math.PI * 2, true);
+          context.closePath();
+          context.stroke();
+        },
+        bubble_float: function () {
+          // Stop animating when it floats past
+          // the top of the page and kill the bubble
+          if (this.centerPoint.y < 0) {
+            that.removeBubble(this.bubbleId);
+            return;
+          }
+          
+          // Raise the position a little bit
+          this.centerPoint.y -= this.distance();          
+        }
+      };
+    },
+    drawBubbles: function () {
+      this.each(
+        function (b) { if (b) { b.drawBubble(); } },
+        this.bubble_holder
+      );
+    },
+    resetHeight: function () {
+      var canvas = this.beer_canvas;
+      canvas.setAttribute('width', document.body.offsetWidth);
+      canvas.setAttribute('height', document.body.offsetHeight);
+      this.canvas_bottom = canvas.height;
+    },
+    initBubbles: function () {
+      var that = this;
+      master_bubble_interval = setInterval(
+        function () {
+          var i;
+          // Make some new bubbles
+          for (i = 0; i < that.num_bubbles_at_a_time; i += 1) {
+            var bubble, bubbleId, intervalId;
+            
+            // Grab an id
+            bubbleId = that.bubble_id_count;
+            that.bubble_id_count += 1;
+            
+            // Create a new bubble and add it to the holder
+            var bubble = that.makeBubble(
+              bubbleId,
+              that.makePoint(
+                that.generateXValue(),
+                that.generateYValue()
+              ),
+              that.generateWidth()
+            );
+            
+            that.addBubble(bubble);
+            that.clearCanvas();
+            that.each(
+              function (b) {
+                if (b) {
+                  b.bubble_float();
+                  b.drawBubble();
+                }
+              },
+              that.bubble_holder
+            );
+          }
+        },
+        this.timeInterval()
+      );
+    },
+    commenceCarbonation: function () {
+      this.resetHeight();
+      this.CANVAS_X_MAX = this.beer_canvas.width;
+      this.CANVAS_Y_MAX = this.beer_canvas.height;
+      var that = this;
+      $(window).resize(function () {
+        that.clearCanvas();
+        that.resetHeight();
+        that.CANVAS_X_MAX = that.beer_canvas.width;
+        that.CANVAS_Y_MAX = that.beer_canvas.height;
+        that.drawBubbles();
+      });
       
-    $(window).resize(function () {
-      clearCanvas();
-      resetHeight();
-      
-      bubble_ns.CANVAS_X_MAX = beer_canvas.width;
-      bubble_ns.CANVAS_Y_MAX = beer_canvas.height;
-      
-      drawBubbles();
-    });
-      
-    beer_context = beer_canvas.getContext('2d');
-    beer_context.strokeStyle = '#161001';
-    beer_context.lineWidth = 1;    
-    init_bubbles();
+      this.beer_context = this.beer_canvas.getContext('2d');
+      this.beer_context.strokeStyle = '$161001';
+      this.beer_context.lineWidth = 1;
+      this.initBubbles();
+    },
+    beerBubbleToggle: function () {
+      if (master_bubble_interval && master_bubble_interval > 0) {
+        clearInterval(master_bubble_interval);
+        this.clearCanvas();
+        master_bubble_interval = 0;
+      } else {
+        this.commenceCarbonation();
+      }
+    },
+    drawBubbles: function () {
+      this.each(
+        function (b) { if (b) { b.drawBubble(); } },
+        this.bubble_holder
+      );
+    }
+  };    
   
-}
-
-var beerBubbleToggle = function () {
-  if(master_bubble_interval && master_bubble_interval > 0) {    
-    clearInterval(master_bubble_interval);
-    clearCanvas();
-    master_bubble_interval = 0;
-  } else {
-    commenceCarbonation();
-  }
-}
+  $(function () {
+    // Insert the CSS
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '#bubble_canvas{ position: absolute; z-index: -1; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    
+    // Insert Canvas into content area
+    var canvas = document.createElement('canvas');
+    canvas.id = 'bubble_canvas';
+    document.body.insertBefore(canvas, document.body.childNodes[0]);
+    
+    beer_framework.beer_canvas = document.getElementById('bubble_canvas');
+    
+    // Check for canvas support
+    if (beer_framework.beer_canvas.getContext) {
+      beer_framework.commenceCarbonation();
+      
+      // Figure out a better way to do this
+      if ($('#bubble_toggle_box')) {
+        $('#bubble_toggle_box').change(function () {
+          beer_framework.beerBubbleToggle(); 
+        });
+      }
+    }
+    debug_framework = beer_framework;
+  });
+})();
